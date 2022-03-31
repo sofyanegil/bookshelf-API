b7c5843a5bb67eb832df005b60f4bcdccc4e9e5f
Sofyan Egi Lesmana <sofyanegil@gmail.com>
2022-03-31 16:56:12 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ const init = async () => {
   server.route(routes);
 
   await server.start();
-  console.log(`Server berjalan pada url berikut ${server.info.uri}`);
+  console.log(`Server berjalan pada url berikut ${server.info.uri}/books`);
 };
 
 init();
